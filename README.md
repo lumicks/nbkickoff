c42032619aa510c9580fdc084aab997c3fe14ea3
Onno Broekmans <o.broekmans@lumicks.com>
2018-06-14 11:40:04 +0200

    Initial project structure
    Basic package structure, installable with `pip`.

---
+++ README.md
@@ -0,0 +1,4 @@
+# nbkickoff
+
+Launch an IPython Notebook from a template notebook file.
+
---
+++ nbkickoff/__init__.py
@@ -0,0 +1,4 @@
+"""Launch an IPython Notebook from a template notebook file"""
+
+from .__about__ import (__author__, __copyright__, __doc__, __email__, __license__, __summary__,
+                        __title__, __url__, __version__)
